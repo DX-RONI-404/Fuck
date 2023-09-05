42dada2bb519498a18c21dc220224c0ef37b6a20
DX-RONI-404 <143085460+DX-RONI-404@users.noreply.github.com>
2023-09-06 02:16:21 +0600

    Add files via upload

--- unlimited .py
+++ unlimited .py
@@ -18,8 +18,23 @@ cps = []
 ugen=[]
 cokbrut=[]
 ses=requests.Session()
+ugen2=[]
 ugen=[]
-ugen=[]
+for xd in range(10000):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
 for ng in range(10000):
 	a='Mozilla/5.0 (Linux; Android'
 	b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
